0d8269761f2988adac55e6c7a5ac2d13e46e7f68
tammeusb <tammeusb@gmail.com>
2024-03-03 14:26:54 -0500

    Initial commit
    Created basic flex layout and javascript to make
    16 divs within content section

---
+++ script.js
@@ -0,0 +1,7 @@
+const containerSketch = document.querySelector('.container-sketch');
+const sketchPixel = document.createElement('div');
+sketchPixel.className = 'sketch-pixel';
+
+for (let i = 0; i < 16; i++) {
+    containerSketch.appendChild(sketchPixel.cloneNode(true));
+}
