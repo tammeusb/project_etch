8a0149ee5541b23047b199909e0719d2cf54f816
tammeusb <tammeusb@gmail.com>
2024-03-03 17:40:05 -0500

    Fix input value - change to per side, not total!
    I had a feeling that this exercise was a little too
    difficult and it turns out I'd already done what
    I was supposed to.
    
    Removed Math.sqrt() method and changed condition
    for generation to multiply input number by itself.
    
    You'll never have a number of squares that can't
    be divided by itself if the input number is the side!
    
    Thank goodness.

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const containerSketch = document.querySelector('.container-sketch');
 const sketchPixel = document.createElement('div');
 sketchPixel.className = 'sketch-pixel';
 
+//Initial generation
+
 for (let i = 0; i < 16; i++) {
-    containerSketch.appendChild(sketchPixel.cloneNode(true));
     sketchPixel.style.flexBasis = '237px';
+    containerSketch.appendChild(sketchPixel.cloneNode(true));
 }
 
 const sketchList = document.querySelectorAll('.sketch-pixel');
@@ -16,21 +18,23 @@ sketchList.forEach((div) => {
     })
 });
 
+//Additional re-generations
+
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', setNumber);
 
 function setNumber() {
-    const sketchNumber = prompt("How many squares?");
+    const sketchNumber = prompt("How many squares per side?");
     document.querySelectorAll('.sketch-pixel').forEach(e => e.remove());
 
-    for (let i = 0; i < sketchNumber; i++) {
+    for (let i = 0; i < (sketchNumber * sketchNumber); i++) {
         containerSketch.appendChild(sketchPixel.cloneNode(true));
     }
 
     document.querySelectorAll('.sketch-pixel').forEach((div) => {
-        div.style.height = `${960 / Math.sqrt(sketchNumber) - 3}px`;
-        div.style.flexBasis = `${960 / Math.sqrt(sketchNumber) - 3}px`;
+        div.style.height = `${960 / sketchNumber - 3}px`;
+        div.style.flexBasis = `${960 / sketchNumber - 3}px`;
         div.addEventListener('mouseover', (e) => {
             e.target.style.backgroundColor = 'red';
         })
