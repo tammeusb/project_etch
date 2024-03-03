bddaa01eec744a8baf4f4d1646d3615e90019e2b
tammeusb <tammeusb@gmail.com>
2024-03-03 14:37:10 -0500

    Add sketch effect

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ for (let i = 0; i < 16; i++) {
     containerSketch.appendChild(sketchPixel.cloneNode(true));
 }
 
+const sketchList = document.querySelectorAll('.sketch-pixel');
+
+sketchList.forEach((div) => {
+    div.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'red';
+    })
+});
