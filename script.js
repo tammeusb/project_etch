002c29cd201f163a3248a9bd47f23ed7d368c4ca
tammeusb <tammeusb@gmail.com>
2024-03-04 11:33:26 -0500

    Clean and rearrange script.js functions

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
+//UI
 const content = document.querySelector('.content');
 const containerSketch = document.querySelector('.container-sketch');
 const sketchPixel = document.createElement('div');
 sketchPixel.className = 'sketch-pixel';
 
-//Initial generation
-
-for (let i = 0; i < 16; i++) {
-    sketchPixel.style.flexBasis = '237px';
-    containerSketch.appendChild(sketchPixel.cloneNode(true));
-}
-
-const sketchList = document.querySelectorAll('.sketch-pixel');
-
-sketchList.forEach((div) => {
-    div.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'red';
-    })
-});
-
-//Additional re-generations
-
 const btn = document.querySelector('button');
 
-btn.addEventListener('click', setNumber);
+let sketchNumber = 16;
 
-function setNumber() {
-    const sketchNumber = parseInt(prompt("How many squares per side?", "up to 100"));
+function getDimension() {
     console.log(typeof sketchNumber);
     if (sketchNumber > 100) {
         alert("too high!");
@@ -33,19 +16,32 @@ function setNumber() {
     } else if (isNaN(sketchNumber)) {
         alert("Please enter a number.");
         return "ERROR"
-    } 
-    document.querySelectorAll('.sketch-pixel').forEach(e => e.remove());
-
-    for (let i = 0; i < (sketchNumber * sketchNumber); i++) {
-        containerSketch.appendChild(sketchPixel.cloneNode(true));
+    } else {
+        renderGrid(sketchNumber);
     }
-
-    document.querySelectorAll('.sketch-pixel').forEach((div) => {
-        div.style.height = `${960 / sketchNumber - 3}px`;
-        div.style.flexBasis = `${960 / sketchNumber - 3}px`;
-        div.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = 'red';
-        })
-    });
-    
 }
+
+function renderGrid(sketchNumber) {
+
+        //delete previous grid
+        containerSketch.innerHTML = '';
+
+        for (let i = 0; i < (sketchNumber * sketchNumber); i++) {
+            containerSketch.appendChild(sketchPixel.cloneNode(true));
+        }
+    
+        document.querySelectorAll('.sketch-pixel').forEach((div) => {
+            div.style.height = `${960 / sketchNumber - 3}px`;
+            div.style.flexBasis = `${960 / sketchNumber - 3}px`;
+            div.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = 'red';
+            })
+        });
+}
+
+btn.addEventListener('click', () => {
+    sketchNumber = parseInt(prompt("How many squares per side?", "up to 100"));
+    getDimension(sketchNumber);
+});
+
+renderGrid(16);
